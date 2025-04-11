0306f57e394637fc93d4e43733ebf994ce0228e5
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-04-11 10:58:11 +0000

    Update Subnational_Level_Temperature_Forecaster.py

--- pages/Subnational_Level_Temperature_Forecaster.py
+++ pages/Subnational_Level_Temperature_Forecaster.py
@@ -17,6 +17,7 @@ st.image(image, width=200)  # Adjust the width as needed
 
 # Title
 st.title("Regions Level Temperature Forecasting App")
+st.write('Curious about how temperature will vary in your region in the future? Please select your country and region.')
 
 # Footer section for Methodology
 st.markdown("---")
