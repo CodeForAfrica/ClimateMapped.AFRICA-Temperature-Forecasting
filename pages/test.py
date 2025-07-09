430610b0652b2d12908fd4380c2f19db1dd931d1
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-07-09 11:18:48 +0000

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -375,8 +375,8 @@ with st.container():
     <div class="sdg-header" style="text-align:center;">
         🎯 Supporting UN Sustainable Development Goals 🎯 <br><br>
         
-        <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Sustainable_Development_Goal_13.png" alt="SDG 13" style="width:100px; height:auto; margin-right:15px;" /> 
-    <img src="https://upload.wikimedia.org/wikipedia/commons/4/44/Sustainable_Development_Goal_11.png" alt="SDG 11" style="width:100px; height:auto; margin-right:15px;" /> 
+    <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Sustainable_Development_Goal_13.png" alt="SDG 13" style="width:100px; height:auto;" /> 
+    <img src="https://upload.wikimedia.org/wikipedia/commons/4/44/Sustainable_Development_Goal_11.png" alt="SDG 11" style="width:100px; height:auto;" />
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Sustainable_Development_Goal_17.png/1200px-Sustainable_Development_Goal_17.png" alt="SDG 17" style="width:100px; height:auto;" />
 
     </div>
