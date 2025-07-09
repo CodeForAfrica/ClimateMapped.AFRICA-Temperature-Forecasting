f4fbea3558d728d98ee3ae1d702fa6d5177ad87c
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-07-09 11:14:42 +0000

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -372,18 +372,15 @@ st.markdown("""
 
 with st.container():
     st.markdown("""
-        <div class="sdg-header">
-            🎯 Supporting UN Sustainable Development Goals 🎯 <br>
-            
-            <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Sustainable_Development_Goal_13.png" alt="SDG 13" style="width:100px; height:auto;" /> 
-             <img src="https://wmo.int/sites/default/files/2023-03/E-WEB-Goal-11.png" alt="SDG 11" style="width:100px; height:auto;" /> 
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Sustainable_Development_Goal_17.png/1200px-Sustainable_Development_Goal_17.png" alt="SDG 17" style="width:100px; height:auto;" /> 
+    <div class="sdg-header" style="text-align:center;">
+        🎯 Supporting UN Sustainable Development Goals 🎯 <br><br>
+        
+        <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Sustainable_Development_Goal_13.png" alt="SDG 13" style="width:100px; height:auto; margin-right:15px;" /> 
+        <img src="https://wmo.int/sites/default/files/2023-03/E-WEB-Goal-11.png" alt="SDG 11" style="width:100px; height:auto; margin-right:15px;" /> 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Sustainable_Development_Goal_17.png/1200px-Sustainable_Development_Goal_17.png" alt="SDG 17" style="width:100px; height:auto;" /> 
+    </div>
+""", unsafe_allow_html=True)
 
-             
-
-
-        </div>
-    """, unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns(3)
     with col1:
