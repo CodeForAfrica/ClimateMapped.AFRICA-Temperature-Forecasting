ce0d2d61c71e28dda7fc35b9d776164ecc9d8bf0
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-07-10 20:20:58 +0000

    Update Climate Map Dashboard.py

--- pages/Climate Map Dashboard.py
+++ pages/Climate Map Dashboard.py
@@ -516,7 +516,7 @@ fig_map.update_layout(height=700, width = 1500, margin=dict(l=0, r=0, t=30, b=0)
 fig_map.update_layout(
     coloraxis_colorbar=dict(
         title="Average Temperature(°C) 2025", 
-        x=0.95,                 # Position on x-axis (0 to 1), right side
+        x=0.45,                 # Position on x-axis (0 to 1), right side
         y=0.05,                 # Position on y-axis (0 to 1), bottom
         xanchor='left',
         yanchor='bottom',
