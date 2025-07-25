9729a99789d107e9c5150bcebb18e34fb754b869
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-07-25 12:59:40 +0000

    Update Climate Map Dashboard.py

--- pages/Climate Map Dashboard.py
+++ pages/Climate Map Dashboard.py
@@ -37,8 +37,6 @@ st.markdown("""
             background-position: center;
             #background: linear-gradient(135deg, #0000FF 0%, #0000FF 100%);
             padding: 30px;
-            margin-left: -3rem;
-            margin-right: -3rem;
             border-radius: 15px;
             text-align: center;
             color: white;
