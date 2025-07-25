bc472a827f8122679d7d62a00bcfb9324d05973f
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-07-25 11:35:18 +0000

    Update Climate Map Dashboard.py

--- pages/Climate Map Dashboard.py
+++ pages/Climate Map Dashboard.py
@@ -92,16 +92,24 @@ st.markdown("""
         #}
 
         .climate-info {
-        background-image: url("https://climatemapped-africa.dev.codeforafrica.org/media/sat-mtKenya-2@2x-1.jpg");
+        background-image: url("https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0");
         background-size: cover;
         background-position: center;
-        padding: 15px;
-        border-radius: 8px;
-        color: white;
+        padding: 30px;
+        border-radius: 10px;
         margin: 15px 0;
         box-shadow: 0 4px 15px rgba(78,205,196,0.3);
-        }
-        
+        color: black;
+        position: relative;
+    }
+    
+    .climate-info .content-box {
+        background: rgba(255, 255, 255, 0.7);  /* cadre blanc transparent */
+        padding: 20px;
+        border-radius: 8px;
+        display: inline-block;
+    }
+            
         .climate-good {
             background: linear-gradient(135deg, #56ab2f 0%, #a8e6cf 100%);
             padding: 15px;
