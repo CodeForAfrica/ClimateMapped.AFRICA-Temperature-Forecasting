dddf6b7f5ca752970c25a586eccd00183011ebfc
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-08-07 12:16:13 +0000

    Update scrollytelling_test.py

--- pages/scrollytelling_test.py
+++ pages/scrollytelling_test.py
@@ -71,7 +71,7 @@ html_code = """
     }
 
     .step.is-active {
-    background-color: transparent;  /* plus de jaune */
+    background-color: transparent !important;  /* plus de jaune */
     color: #3b3b3b;
     border-color: goldenrod;
   }
