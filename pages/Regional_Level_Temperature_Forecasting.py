a744aad7dcacfd4bab9c426b6b22e748e77e65e4
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-12-02 14:31:38 +0100

    Update Regional_Level_Temperature_Forecasting.py

--- pages/Regional_Level_Temperature_Forecasting.py
+++ pages/Regional_Level_Temperature_Forecasting.py
@@ -114,66 +114,62 @@ future_city['y'] = future_city['y'].round(2)
 
 # Plot Line Chart 
 # ---------------------------
+
+# Historical + forecast for a single city
 combined = pd.concat([df_city, future_city])
+combined['year_float'] = combined['ds'].dt.year + (combined['ds'].dt.month-1)/12
 
-combined['year_float'] = combined['ds'].dt.year + (combined['ds'].dt.month - 1) / 12
-
+# Trend line
 z = np.polyfit(combined['year_float'], combined['y'], 1)
 p = np.poly1d(z)
 combined['trend'] = p(combined['year_float'])
 
-y_min = 4
-y_max = 45
-
-
-st.subheader("Historical vs Predicted Temperature")
-
 fig = go.Figure()
 
-# Historical data
+# Historical
+y_min_hist = df_city['y'].min()
+y_max_hist = df_city['y'].max()
 fig.add_trace(go.Scatter(
-    x=df_city["ds"], 
-    y=df_city["y"], 
-    mode="lines",
+    x=df_city["ds"],
+    y=df_city["y"],
+    mode="lines+markers",
     name="Historical",
     line=dict(color="blue")
 ))
 
 # Forecast
+y_min_forecast = future_city['y'].min()
+y_max_forecast = future_city['y'].max()
 fig.add_trace(go.Scatter(
-    x=future_city["ds"], 
-    y=future_city["y"], 
-    mode="lines",
+    x=future_city["ds"],
+    y=future_city["y"],
+    mode="lines+markers",
     name="Forecast",
     line=dict(color="red", dash='dot')
 ))
 
-# Trend line (continue sur tout, style interrompu sur forecast)
+# Trend line
 fig.add_trace(go.Scatter(
     x=combined["ds"],
     y=combined["trend"],
     mode="lines",
     name="Trend",
-    line=dict(color="green", width=3, dash='dash'),
-    hovertemplate='Date: %{x|%b %Y}<br>Trend: %{y:.2f}°C<extra></extra>'
+    line=dict(color="green", width=3, dash='dash')
 ))
 
-fig.update_yaxes(range=[23, max(combined['y'].max(), combined['trend'].max()) + 1])
+# Set individual Y-axis ranges
+fig.update_yaxes(range=[min(y_min_hist, y_min_forecast), max(y_max_hist, y_max_forecast)])
 
-# Layout
 fig.update_layout(
     xaxis_title="Date",
     yaxis_title="Temperature (°C)",
-    height=500,
     template="plotly_white",
-    legend=dict(orientation="h", y=1.1)
-)
-fig.update_yaxes(
-    range=[y_min, y_max], 
-    title_text="Temperature (°C)"
+    legend=dict(orientation="h", y=1.1),
+    height=500
 )
 
 st.plotly_chart(fig, use_container_width=True)
+
 
 
 # HEATMAP
